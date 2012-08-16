d1b663ccdb0809dea430e080b7254e52a97f3d40
amdixon <alexander.m.dixon@gmail.com>
2012-08-15 21:48:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmdixonSite::Application.config.secret_token = 'e6de7d10d0236fd40ed8c968a9cabb8b4fd7e423adea0df55fb2143c4283e9b97abb72d57081ce455b65c5b7d462f91ced74884144f44ee16e9006407206693e'
